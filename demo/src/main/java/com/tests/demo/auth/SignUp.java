5da7db3c6c919e9ada942892820bbb875fa9d53d
Carlos Maina Wanjiku <softwarewebdevelopers1@gmail.com>
2026-06-17 11:55:12 +0300

    removed unnecessary field List

--- demo/src/main/java/com/tests/demo/auth/SignUp.java
+++ demo/src/main/java/com/tests/demo/auth/SignUp.java
@@ -1,7 +1,5 @@
 package com.tests.demo.auth;
 
-import java.util.List;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
