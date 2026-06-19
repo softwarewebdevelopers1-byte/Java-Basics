3891c9de5a4a3be86db21cbd3d707f5e456d4f9a
Carlos Maina Wanjiku <softwarewebdevelopers1@gmail.com>
2026-06-19 09:09:42 +0300

    request param annotation example

--- demo/src/main/java/com/tests/demo/controller/GetSubjects.java
+++ demo/src/main/java/com/tests/demo/controller/GetSubjects.java
@@ -3,12 +3,19 @@ package com.tests.demo.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class GetSubjects {
     @GetMapping("/subjects/{userId}")
     public String subjects(@PathVariable("userId") String userId) {
         return userId;
+    }
+
+    // http://localhost:8000/subjects?subjectId=english&userId=2
+    @GetMapping("/subjects")
+    public String getMethodName(@RequestParam("subjectId") String subjectId, @RequestParam("userId") String userId) {
+        return subjectId + " " + userId;
     }
 
 }
