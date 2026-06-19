7f08264d24e17a951b101f211084c534de75d7eb
Carlos Maina Wanjiku <softwarewebdevelopers1@gmail.com>
2026-06-19 10:25:18 +0300

    getting all students route example added

--- demo/src/main/java/com/tests/demo/controller/FindStudent.java
+++ demo/src/main/java/com/tests/demo/controller/FindStudent.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tests.demo.models.Students;
 import com.tests.demo.types.StudentsInterface;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -16,9 +18,15 @@ public class FindStudent {
         this.student = studentsInterface;
     }
 
+    // findind single student by id
     @GetMapping("/get/student/{id}")
     public Students GetStudents(@PathVariable("id") Long id) {
         return student.findById(id).orElse(new Students());
     }
 
+    // finding all students
+    @GetMapping("/get/all/students")
+    public List<Students> GetAllStudents() {
+        return student.findAll();
+    }
 }
